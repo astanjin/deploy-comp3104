d6c2d69affeabb637f6605df4a99ca23dbc6f2f5
astanjin <mina.astanjin@gmail.com>
2021-10-29 02:48:05 -0400

    updated

---
+++ build/precache-manifest.52b3130121be78f06d6fff609ebafd9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1447ef7aaa44c7b69cf1",
+    "url": "/astanjin/deploy-comp3104/static/css/main.66584f8e.chunk.css"
+  },
+  {
+    "revision": "1447ef7aaa44c7b69cf1",
+    "url": "/astanjin/deploy-comp3104/static/js/main.1447ef7a.chunk.js"
+  },
+  {
+    "revision": "ff64e09ed741d23fea79",
+    "url": "/astanjin/deploy-comp3104/static/js/runtime~main.ff64e09e.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/astanjin/deploy-comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "56852d755a0bf4c22a0b40e7fbabc68e",
+    "url": "/astanjin/deploy-comp3104/index.html"
+  }
+];
